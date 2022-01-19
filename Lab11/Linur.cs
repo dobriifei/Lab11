ff9252223a2639fa79572be9857a5912ef64dc2d
vermei2@outlook.com <vermei2@outlook.com>
2022-01-19 22:13:39 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Linur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    struct Linur
+    {
+        double k;
+        double b;
+        public Linur(double k, double b)
+        { this.k = k;
+            this.b = b; }
+        public string Root ()
+        {double x ;
+            if (k < 0 || k > 0)
+            {
+                x = -b / k;
+                return $"х равен - {x}"; 
+            }
+            else
+                return "х-любое число";
+        }
+    }     
+}
+
+
---
+++ Lab11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linur linur = new Linur(k, b);
+            Console.WriteLine(linur.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
